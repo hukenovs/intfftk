b9366883d17c4cb38591e2d54b9fabceabfff298
Alexander Kapitanov <sallador@bk.ru>
2018-11-24 15:10:13 +0300

    Alignment and indentation

--- src/verilog/math/mults/mlt35x27_dsp48e2.v
+++ src/verilog/math/mults/mlt35x27_dsp48e2.v
@@ -9,7 +9,7 @@
 //
 //-----------------------------------------------------------------------------
 // 
-//	Version 1.0: 14.02.2018
+//    Version 1.0: 14.02.2018
 //
 //  Description: Double complex multiplier by DSP48 unit
 //
@@ -20,8 +20,8 @@
 //    B port - data width up to 35 bits
 //  * - 25 bits for DSP48E1, 27 bits for DSP48E2.
 //
-//  Total delay			: 4 clock cycles,
-//  Total resources		: 2 DSP48 units
+//  Total delay            : 4 clock cycles,
+//  Total resources        : 2 DSP48 units
 //
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -52,160 +52,160 @@
 //-----------------------------------------------------------------------------
 
 module mlt35x27_dsp48e2
-	(
-		input   RST, CLK,
-		input  [34:0] MLT_A,
-		input  [26:0] MLT_B,
-		output reg [61:0] MLT_P
-	);
+    (
+        input   RST, CLK,
+        input  [34:0] MLT_A,
+        input  [26:0] MLT_B,
+        output reg [61:0] MLT_P
+    );
 
-	wire [29 : 0] dspA_12;
-	wire [17 : 0] dspB_M1;
-	wire [17 : 0] dspB_M2;
-	wire [47 : 0] dspP_M1;
-	wire [47 : 0] dspP_M2;
-	wire [47 : 0] dspP_12;
+    wire [29 : 0] dspA_12;
+    wire [17 : 0] dspB_M1;
+    wire [17 : 0] dspB_M2;
+    wire [47 : 0] dspP_M1;
+    wire [47 : 0] dspP_M2;
+    wire [47 : 0] dspP_12;
 
-	always @(posedge CLK) begin  
-		MLT_P[16 : 00] <= dspP_M2[16 : 00];
-	end
-	always @(*) begin
+    always @(posedge CLK) begin  
+        MLT_P[16 : 00] <= dspP_M2[16 : 00];
+    end
+    always @(*) begin
         MLT_P[61 : 17] <= dspP_M1[44 : 00];
     end
 
-	assign dspA_12[26 : 00] = MLT_B[26 : 0];
-	assign dspA_12[29 : 27] = {3{MLT_B[26]}};
-	
-	assign dspB_M2[16 : 00] = MLT_A[16 : 00];
-	assign dspB_M2[17] = 1'b0;
-	assign dspB_M1[17 : 00] = MLT_A[34 : 17];
+    assign dspA_12[26 : 00] = MLT_B[26 : 0];
+    assign dspA_12[29 : 27] = {3{MLT_B[26]}};
+    
+    assign dspB_M2[16 : 00] = MLT_A[16 : 00];
+    assign dspB_M2[17] = 1'b0;
+    assign dspB_M1[17 : 00] = MLT_A[34 : 17];
 
     // Wrap DSP48E2 unit
-	DSP48E2 #(
-		.USE_MULT("MULTIPLY"),  
-		.ACASCREG(1),           
-		.ADREG(1),              
-		.ALUMODEREG(1),         
-		.AREG(2),               
-		.BCASCREG(1),           
-		.BREG(2),               
-		.CARRYINREG(1),         
-		.CARRYINSELREG(1),      
-		.CREG(1),               
-		.DREG(1),               
-		.INMODEREG(1),          
-		.MREG(1),               
-		.OPMODEREG(1),          
-		.PREG(1)                
-	)
-    xDSP_M1 (                 
-       // Cascade: 30-bit (each) input: 
-       .ACIN(30'b0),                     
-       .BCIN(18'b0),                     
-       .CARRYCASCIN(1'b0),       
-       .MULTSIGNIN(1'b0),         
-       .PCIN(dspP_12),
-       .ALUMODE(4'b0),
-       .CARRYINSEL(3'b0),
-       .CLK(CLK),
-       .INMODE(5'b0),
-       .OPMODE(9'b001010101),
-       // Data input / output data ports
-       .A(dspA_12),
-       .B(dspB_M1),
-       .C(48'b0),
-       .P(dspP_M1),
-       .D(27'b0),
-       .CARRYIN(1'b0),
-       // Clock enables
-       .CEA1(1'b1),     
-       .CEA2(1'b1),     
-       .CEAD(1'b1),     
-       .CEALUMODE(1'b1),
-       .CEB1(1'b1),     
-       .CEB2(1'b1),     
-       .CEC(1'b1),      
-       .CECARRYIN(1'b1),
-       .CECTRL(1'b1),   
-       .CED(1'b1),      
-       .CEINMODE(1'b1), 
-       .CEM(1'b1),
-       .CEP(1'b1),
-       .RSTA(RST),
-       .RSTALLCARRYIN(RST),   
-       .RSTALUMODE(RST),
-       .RSTB(RST),
-       .RSTC(RST),
-       .RSTCTRL(RST),
-       .RSTD(RST),
-       .RSTINMODE(RST),
-       .RSTM(RST),
-       .RSTP(RST)
+    DSP48E2 #(
+        .USE_MULT("MULTIPLY"),  
+        .ACASCREG(1),
+        .ADREG(1),
+        .ALUMODEREG(1),
+        .AREG(2),
+        .BCASCREG(1),
+        .BREG(2),
+        .CARRYINREG(1),
+        .CARRYINSELREG(1),  
+        .CREG(1),
+        .DREG(1),
+        .INMODEREG(1), 
+        .MREG(1),
+        .OPMODEREG(1), 
+        .PREG(1)
+    )
+    xDSP_M1 (
+        // Cascade: 30-bit (each) input: 
+        .ACIN(30'b0), 
+        .BCIN(18'b0), 
+        .CARRYCASCIN(1'b0),
+        .MULTSIGNIN(1'b0),
+        .PCIN(dspP_12),
+        .ALUMODE(4'b0),
+        .CARRYINSEL(3'b0),
+        .CLK(CLK),
+        .INMODE(5'b0),
+        .OPMODE(9'b001010101),
+        // Data input / output data ports
+        .A(dspA_12),
+        .B(dspB_M1),
+        .C(48'b0),
+        .P(dspP_M1),
+        .D(27'b0),
+        .CARRYIN(1'b0),
+        // Clock enables
+        .CEA1(1'b1), 
+        .CEA2(1'b1), 
+        .CEAD(1'b1), 
+        .CEALUMODE(1'b1),
+        .CEB1(1'b1), 
+        .CEB2(1'b1), 
+        .CEC(1'b1),  
+        .CECARRYIN(1'b1),
+        .CECTRL(1'b1),
+        .CED(1'b1),  
+        .CEINMODE(1'b1), 
+        .CEM(1'b1),
+        .CEP(1'b1),
+        .RSTA(RST),
+        .RSTALLCARRYIN(RST),
+        .RSTALUMODE(RST),
+        .RSTB(RST),
+        .RSTC(RST),
+        .RSTCTRL(RST),
+        .RSTD(RST),
+        .RSTINMODE(RST),
+        .RSTM(RST),
+        .RSTP(RST)
     );
 
     // Wrap DSP48E1 unit
-	DSP48E2 #(
-		.USE_MULT("MULTIPLY"),  
-		.ACASCREG(1),           
-		.ADREG(1),              
-		.ALUMODEREG(1),         
-		.AREG(1),               
-		.BCASCREG(1),           
-		.BREG(1),               
-		.CARRYINREG(1),         
-		.CARRYINSELREG(1),      
-		.CREG(1),               
-		.DREG(1),               
-		.INMODEREG(1),          
-		.MREG(1),               
-		.OPMODEREG(1),          
-		.PREG(1)                
-	)
-    xDSP_M2 (                 
-       // Cascade: 30-bit (each) input: 
-       .ACIN(30'b0),                     
-       .BCIN(18'b0),                     
-       .CARRYCASCIN(1'b0),       
-       .MULTSIGNIN(1'b0),         
-       .PCIN(48'b0),
-       .PCOUT(dspP_12), 
-       .ALUMODE(4'b0),
-       .CARRYINSEL(3'b0),
-       .CLK(CLK),
-       .INMODE(5'b0),
-       .OPMODE(9'b000000101),
-       // Data input / output data ports
-       .A(dspA_12),
-       .B(dspB_M2),
-       .C(48'b0),
-       .P(dspP_M2),
+    DSP48E2 #(
+        .USE_MULT("MULTIPLY"),  
+        .ACASCREG(1),
+        .ADREG(1),
+        .ALUMODEREG(1),
+        .AREG(1),
+        .BCASCREG(1),
+        .BREG(1),
+        .CARRYINREG(1),
+        .CARRYINSELREG(1),  
+        .CREG(1),
+        .DREG(1),
+        .INMODEREG(1), 
+        .MREG(1),
+        .OPMODEREG(1), 
+        .PREG(1)
+    )
+    xDSP_M2 (
+        // Cascade: 30-bit (each) input: 
+        .ACIN(30'b0), 
+        .BCIN(18'b0), 
+        .CARRYCASCIN(1'b0),
+        .MULTSIGNIN(1'b0),
+        .PCIN(48'b0),
+        .PCOUT(dspP_12), 
+        .ALUMODE(4'b0),
+        .CARRYINSEL(3'b0),
+        .CLK(CLK),
+        .INMODE(5'b0),
+        .OPMODE(9'b000000101),
+        // Data input / output data ports
+        .A(dspA_12),
+        .B(dspB_M2),
+        .C(48'b0),
+        .P(dspP_M2),
 
-       .D(27'b0),
-       .CARRYIN(1'b0),
-       // Clock enables
-       .CEA1(1'b1),     
-       .CEA2(1'b1),     
-       .CEAD(1'b1),     
-       .CEALUMODE(1'b1),
-       .CEB1(1'b1),     
-       .CEB2(1'b1),     
-       .CEC(1'b1),      
-       .CECARRYIN(1'b1),
-       .CECTRL(1'b1),   
-       .CED(1'b1),      
-       .CEINMODE(1'b1), 
-       .CEM(1'b1),
-       .CEP(1'b1),
-       .RSTA(RST),
-       .RSTALLCARRYIN(RST),   
-       .RSTALUMODE(RST),
-       .RSTB(RST),
-       .RSTC(RST),
-       .RSTCTRL(RST),
-       .RSTD(RST),
-       .RSTINMODE(RST),
-       .RSTM(RST),
-       .RSTP(RST)
+        .D(27'b0),
+        .CARRYIN(1'b0),
+        // Clock enables
+        .CEA1(1'b1), 
+        .CEA2(1'b1), 
+        .CEAD(1'b1), 
+        .CEALUMODE(1'b1),
+        .CEB1(1'b1), 
+        .CEB2(1'b1), 
+        .CEC(1'b1),  
+        .CECARRYIN(1'b1),
+        .CECTRL(1'b1),
+        .CED(1'b1),  
+        .CEINMODE(1'b1), 
+        .CEM(1'b1),
+        .CEP(1'b1),
+        .RSTA(RST),
+        .RSTALLCARRYIN(RST),
+        .RSTALUMODE(RST),
+        .RSTB(RST),
+        .RSTC(RST),
+        .RSTCTRL(RST),
+        .RSTD(RST),
+        .RSTINMODE(RST),
+        .RSTM(RST),
+        .RSTP(RST)
     );
 
 endmodule
--- src/verilog/math/mults/mlt42x18_dsp48e1.v
+++ src/verilog/math/mults/mlt42x18_dsp48e1.v
@@ -9,7 +9,7 @@
 //
 //-----------------------------------------------------------------------------
 //
-//	Version 1.0: 14.02.2018
+//    Version 1.0: 14.02.2018
 //
 //  Description: Double complex multiplier by DSP48 unit
 //
@@ -20,8 +20,8 @@
 //    B port - data width up to 18 bits
 //  * - 42 bits for DSP48E1, 44 bits for DSP48E2.
 //
-//  Total delay			: 4 clock cycles,
-//  Total resources		: 2 DSP48 units
+//  Total delay        : 4 clock cycles,
+//  Total resources    : 2 DSP48 units
 //
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
@@ -52,161 +52,161 @@
 //-----------------------------------------------------------------------------
 
 module mlt42x18_dsp48e1
-	(
-		input   RST, CLK,
-		input  [41:0] MLT_A,
-		input  [17:0] MLT_B,
-		output reg [59:0] MLT_P
-	);
+    (
+        input   RST, CLK,
+        input  [41:0] MLT_A,
+        input  [17:0] MLT_B,
+        output reg [59:0] MLT_P
+    );
 
-	wire [17 : 0] dspB_12;
-	wire [29 : 0] dspA_M1;
-	wire [29 : 0] dspA_M2;
-	wire [47 : 0] dspP_M1;
-	wire [47 : 0] dspP_M2;
-	wire [47 : 0] dspP_12;
+    wire [17 : 0] dspB_12;
+    wire [29 : 0] dspA_M1;
+    wire [29 : 0] dspA_M2;
+    wire [47 : 0] dspP_M1;
+    wire [47 : 0] dspP_M2;
+    wire [47 : 0] dspP_12;
 
-	always @(posedge CLK) begin  
-		MLT_P[16 : 00] <= dspP_M2[16 : 00];
-	end
-	always @(*) begin
+    always @(posedge CLK) begin  
+        MLT_P[16 : 00] <= dspP_M2[16 : 00];
+    end
+    always @(*) begin
         MLT_P[59 : 17] <= dspP_M1[42 : 00];
     end
 
-	assign dspA_M2[16 : 00] = MLT_A[16 : 0];
-	assign dspA_M2[29 : 17] = {13{1'b0}};
-	
-	assign dspA_M1[24 : 00] = MLT_A[41 : 17];
-	assign dspA_M1[29 : 25] = {5{MLT_A[41]}};
+    assign dspA_M2[16 : 00] = MLT_A[16 : 0];
+    assign dspA_M2[29 : 17] = {13{1'b0}};
+    
+    assign dspA_M1[24 : 00] = MLT_A[41 : 17];
+    assign dspA_M1[29 : 25] = {5{MLT_A[41]}};
 
-	assign dspB_12 = MLT_B;
+    assign dspB_12 = MLT_B;
 
     // Wrap DSP48E1 unit
-	DSP48E1 #(
-		.USE_MULT("MULTIPLY"),  
-		.ACASCREG(1),           
-		.ADREG(1),              
-		.ALUMODEREG(1),         
-		.AREG(2),               
-		.BCASCREG(1),           
-		.BREG(2),               
-		.CARRYINREG(1),         
-		.CARRYINSELREG(1),      
-		.CREG(1),               
-		.DREG(1),               
-		.INMODEREG(1),          
-		.MREG(1),               
-		.OPMODEREG(1),          
-		.PREG(1)                
-	)
-    xDSP_M1 (                 
+    DSP48E1 #(
+        .USE_MULT("MULTIPLY"),
+        .ACASCREG(1), 
+        .ADREG(1), 
+        .ALUMODEREG(1),
+        .AREG(2),
+        .BCASCREG(1), 
+        .BREG(2),
+        .CARRYINREG(1),
+        .CARRYINSELREG(1),
+        .CREG(1),
+        .DREG(1),
+        .INMODEREG(1),
+        .MREG(1),
+        .OPMODEREG(1),
+        .PREG(1)
+    )
+    xDSP_M1 (
        // Cascade: 30-bit (each) input: 
-       .ACIN(30'b0),                     
-       .BCIN(18'b0),                     
-       .CARRYCASCIN(1'b0),       
-       .MULTSIGNIN(1'b0),         
-       .PCIN(dspP_12),
-       .ALUMODE(4'b0),
-       .CARRYINSEL(3'b0),
-       .CLK(CLK),
-       .INMODE(5'b0),
-       .OPMODE(7'b1010101),
-       // Data input / output data ports
-       .A(dspA_M1),
-       .B(dspB_12),
-       .C(48'b0),
-       .P(dspP_M1),
-       .D(25'b0),
-       .CARRYIN(1'b0),
-       // Clock enables
-       .CEA1(1'b1),     
-       .CEA2(1'b1),     
-       .CEAD(1'b1),     
-       .CEALUMODE(1'b1),
-       .CEB1(1'b1),     
-       .CEB2(1'b1),     
-       .CEC(1'b1),      
-       .CECARRYIN(1'b1),
-       .CECTRL(1'b1),   
-       .CED(1'b1),      
-       .CEINMODE(1'b1), 
-       .CEM(1'b1),
-       .CEP(1'b1),
-       .RSTA(RST),
-       .RSTALLCARRYIN(RST),   
-       .RSTALUMODE(RST),
-       .RSTB(RST),
-       .RSTC(RST),
-       .RSTCTRL(RST),
-       .RSTD(RST),
-       .RSTINMODE(RST),
-       .RSTM(RST),
-       .RSTP(RST)
+        .ACIN(30'b0),
+        .BCIN(18'b0),
+        .CARRYCASCIN(1'b0),
+        .MULTSIGNIN(1'b0),
+        .PCIN(dspP_12),
+        .ALUMODE(4'b0),
+        .CARRYINSEL(3'b0),
+        .CLK(CLK),
+        .INMODE(5'b0),
+        .OPMODE(7'b1010101),
+        // Data input / output data ports
+        .A(dspA_M1),
+        .B(dspB_12),
+        .C(48'b0),
+        .P(dspP_M1),
+        .D(25'b0),
+        .CARRYIN(1'b0),
+        // Clock enables
+        .CEA1(1'b1),
+        .CEA2(1'b1),
+        .CEAD(1'b1),
+        .CEALUMODE(1'b1),
+        .CEB1(1'b1),
+        .CEB2(1'b1),
+        .CEC(1'b1),
+        .CECARRYIN(1'b1),
+        .CECTRL(1'b1),
+        .CED(1'b1),
+        .CEINMODE(1'b1), 
+        .CEM(1'b1),
+        .CEP(1'b1),
+        .RSTA(RST),
+        .RSTALLCARRYIN(RST),
+        .RSTALUMODE(RST),
+        .RSTB(RST),
+        .RSTC(RST),
+        .RSTCTRL(RST),
+        .RSTD(RST),
+        .RSTINMODE(RST),
+        .RSTM(RST),
+        .RSTP(RST)
     );
 
     // Wrap DSP48E1 unit
-	DSP48E1 #(
-		.USE_MULT("MULTIPLY"),  
-		.ACASCREG(1),           
-		.ADREG(1),              
-		.ALUMODEREG(1),         
-		.AREG(1),               
-		.BCASCREG(1),           
-		.BREG(1),               
-		.CARRYINREG(1),         
-		.CARRYINSELREG(1),      
-		.CREG(1),               
-		.DREG(1),               
-		.INMODEREG(1),          
-		.MREG(1),               
-		.OPMODEREG(1),          
-		.PREG(1)                
-	)
-    xDSP_M2 (                 
-       // Cascade: 30-bit (each) input: 
-       .ACIN(30'b0),                     
-       .BCIN(18'b0),                     
-       .CARRYCASCIN(1'b0),       
-       .MULTSIGNIN(1'b0),         
-       .PCIN(48'b0),
-       .PCOUT(dspP_12), 
-       .ALUMODE(4'b0),
-       .CARRYINSEL(3'b0),
-       .CLK(CLK),
-       .INMODE(5'b0),
-       .OPMODE(7'b0000101),
-       // Data input / output data ports
-       .A(dspA_M2),
-       .B(dspB_12),
-       .C(48'b0),
-       .P(dspP_M2),
+    DSP48E1 #(
+        .USE_MULT("MULTIPLY"),
+        .ACASCREG(1), 
+        .ADREG(1), 
+        .ALUMODEREG(1),
+        .AREG(1),
+        .BCASCREG(1), 
+        .BREG(1),
+        .CARRYINREG(1),
+        .CARRYINSELREG(1),
+        .CREG(1),
+        .DREG(1),
+        .INMODEREG(1),
+        .MREG(1),
+        .OPMODEREG(1),
+        .PREG(1)
+    )
+    xDSP_M2 (
+        // Cascade: 30-bit (each) input: 
+        .ACIN(30'b0),
+        .BCIN(18'b0),
+        .CARRYCASCIN(1'b0),
+        .MULTSIGNIN(1'b0),
+        .PCIN(48'b0),
+        .PCOUT(dspP_12), 
+        .ALUMODE(4'b0),
+        .CARRYINSEL(3'b0),
+        .CLK(CLK),
+        .INMODE(5'b0),
+        .OPMODE(7'b0000101),
+        // Data input / output data ports
+        .A(dspA_M2),
+        .B(dspB_12),
+        .C(48'b0),
+        .P(dspP_M2),
 
-       .D(25'b0),
-       .CARRYIN(1'b0),
-       // Clock enables
-       .CEA1(1'b1),     
-       .CEA2(1'b1),     
-       .CEAD(1'b1),     
-       .CEALUMODE(1'b1),
-       .CEB1(1'b1),     
-       .CEB2(1'b1),     
-       .CEC(1'b1),      
-       .CECARRYIN(1'b1),
-       .CECTRL(1'b1),   
-       .CED(1'b1),      
-       .CEINMODE(1'b1), 
-       .CEM(1'b1),
-       .CEP(1'b1),
-       .RSTA(RST),
-       .RSTALLCARRYIN(RST),   
-       .RSTALUMODE(RST),
-       .RSTB(RST),
-       .RSTC(RST),
-       .RSTCTRL(RST),
-       .RSTD(RST),
-       .RSTINMODE(RST),
-       .RSTM(RST),
-       .RSTP(RST)
+        .D(25'b0),
+        .CARRYIN(1'b0),
+        // Clock enables
+        .CEA1(1'b1),
+        .CEA2(1'b1),
+        .CEAD(1'b1),
+        .CEALUMODE(1'b1),
+        .CEB1(1'b1),
+        .CEB2(1'b1),
+        .CEC(1'b1),
+        .CECARRYIN(1'b1),
+        .CECTRL(1'b1),
+        .CED(1'b1),
+        .CEINMODE(1'b1), 
+        .CEM(1'b1),
+        .CEP(1'b1),
+        .RSTA(RST),
+        .RSTALLCARRYIN(RST),
+        .RSTALUMODE(RST),
+        .RSTB(RST),
+        .RSTC(RST),
+        .RSTCTRL(RST),
+        .RSTD(RST),
+        .RSTINMODE(RST),
+        .RSTM(RST),
+        .RSTP(RST)
     );
 
 endmodule
