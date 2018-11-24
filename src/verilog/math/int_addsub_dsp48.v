fb9ce57c18c174c6a918c23f4eb092d578946281
Alexander Kapitanov <sallador@bk.ru>
2018-11-25 00:24:21 +0300

    Alignment and indentation (addsub)

--- src/verilog/math/int_addsub_dsp48.v
+++ src/verilog/math/int_addsub_dsp48.v
@@ -1,31 +1,30 @@
 //-----------------------------------------------------------------------------
 //
 // Title       : int_addsub_dsp48
-// Design      : FFTK
-// Author      : Kapitanov
+// Design      : Integer Fast Fourier Transform
+// Author      : Kapitanov Alexander
 // Company     :
 //
-// Description : Integer adder/subtractor on DSP48 block
+// Description : Integer adder/subtractor based on DSP48 block
 //
 //-----------------------------------------------------------------------------
 //
-//    Version 1.0: 12.02.2018
+//  Version 1.0: 22.11.2018
 //
-//  Description: Simple complex adder/subtractor by DSP48 unit
+//  Description: Simple complex adder/subtractor by using DSP48 unit
 //
-//  Math:
-//
+//  Math function:
 //  Out:    In:
 //  OX_RE = IA_RE + IB_RE;
 //  OX_IM = IA_IM + IB_IM; 
 //  OY_RE = IA_RE - IB_RE; 
 //  OY_IM = IA_IM - IB_IM; 
 //
-//    Input variables:
-//    1. DSPW - DSP48 input width (from 8 to 48): data width + FFT stage
-//    2. XSER - Xilinx series: 
-//        "NEW" - DSP48E2 (Ultrascale),
-//        "OLD" - DSP48E1 (6/7-series).
+//  Input variables:
+//  1. DSPW - DSP48 input width (from 8 to 48): data width + FFT stage
+//  2. XSER - Xilinx series: 
+//      "NEW" - DSP48E2 (Ultrascale),
+//      "OLD" - DSP48E1 (6/7-series).
 //
 //  DSP48 data signals:
 //    A port - In B data (MSB part),
@@ -79,33 +78,33 @@
 module int_addsub_dsp48 
     #(
         parameter 
-        DATA_WIDTH  = 16,
-        XSER  = "OLD"
+        DATA_WIDTH = 16,
+        XSER       = "OLD"
     )
     (
-        input CLK,RST,
-        input  signed [DATA_WIDTH-1 : 0] IA_RE,IA_IM,IB_RE,IB_IM,
-        output reg signed [DATA_WIDTH : 0] OX_RE,OX_IM,OY_RE,OY_IM
+        input                                                  CLK, RST,
+        input      signed [DATA_WIDTH-1 : 0] IA_RE, IA_IM, IB_RE, IB_IM,
+        output reg signed [DATA_WIDTH   : 0] OX_RE, OX_IM, OY_RE, OY_IM
     );
     
     generate 
         if ((DATA_WIDTH > 23) & (DATA_WIDTH < 48)) begin
-            wire [29 : 0] dspA_RE,dspA_IM;
-            wire [17 : 0] dspB_RE,dspB_IM;
-            wire [47 : 0] dspC_RE,dspC_IM;
+            wire [29 : 0] dspA_RE, dspA_IM;
+            wire [17 : 0] dspB_RE, dspB_IM;
+            wire [47 : 0] dspC_RE, dspC_IM;
 
-            wire [47 : 0] dspX_RE,dspX_IM,dspY_RE,dspY_IM;
+            wire [47 : 0] dspX_RE, dspX_IM, dspY_RE, dspY_IM;
 
             //-- Create A:B 48-bit data ----
             assign dspB_RE = IB_RE[17 : 00];
             assign dspB_IM = IB_IM[17 : 00];
     
             // -- A port 48-bit data ----
-            assign dspA_RE = { {(30-DATA_WIDTH){IB_RE[DATA_WIDTH-1]}},IB_RE[DATA_WIDTH-1 : 0] };
-            assign dspA_IM = { {(30-DATA_WIDTH){IB_IM[DATA_WIDTH-1]}},IB_IM[DATA_WIDTH-1 : 0] };
+            assign dspA_RE = { {(30-DATA_WIDTH){IB_RE[DATA_WIDTH-1]}}, IB_RE[DATA_WIDTH-1 : 0] };
+            assign dspA_IM = { {(30-DATA_WIDTH){IB_IM[DATA_WIDTH-1]}}, IB_IM[DATA_WIDTH-1 : 0] };
 
-            assign dspC_RE = { {(48-DATA_WIDTH){IA_RE[DATA_WIDTH-1]}},IA_RE[DATA_WIDTH-1 : 0] };
-            assign dspC_IM = { {(48-DATA_WIDTH){IA_IM[DATA_WIDTH-1]}},IA_IM[DATA_WIDTH-1 : 0] };
+            assign dspC_RE = { {(48-DATA_WIDTH){IA_RE[DATA_WIDTH-1]}}, IA_RE[DATA_WIDTH-1 : 0] };
+            assign dspC_IM = { {(48-DATA_WIDTH){IA_IM[DATA_WIDTH-1]}}, IA_IM[DATA_WIDTH-1 : 0] };
         
             always @(*) begin
                 OX_RE = dspX_RE[DATA_WIDTH : 0];
@@ -616,17 +615,17 @@ module int_addsub_dsp48
 
             wire [29 : 0] dspA_XY;
             wire [17 : 0] dspB_XY;
-            wire [47 : 0] dspAB,dspC_XY,dspP_XX,dspP_YY;
+            wire [47 : 0] dspAB, dspC_XY, dspP_XX, dspP_YY;
 
             //-- Create A:B 48-bit data ----
-            assign dspC_XY = { {(24-DATA_WIDTH){IA_IM[DATA_WIDTH-1]}},IA_IM,{(24-DATA_WIDTH){IA_RE[DATA_WIDTH-1]}},IA_RE };
-            assign dspAB   = { {(24-DATA_WIDTH){IB_IM[DATA_WIDTH-1]}},IB_IM,{(24-DATA_WIDTH){IB_RE[DATA_WIDTH-1]}},IB_RE };
+            assign dspC_XY = { {(24-DATA_WIDTH){IA_IM[DATA_WIDTH-1]}}, IA_IM, {(24-DATA_WIDTH){IA_RE[DATA_WIDTH-1]}}, IA_RE };
+            assign dspAB   = { {(24-DATA_WIDTH){IB_IM[DATA_WIDTH-1]}}, IB_IM, {(24-DATA_WIDTH){IB_RE[DATA_WIDTH-1]}}, IB_RE };
             assign dspA_XY = dspAB[47 : 18];
             assign dspB_XY = dspAB[17 : 00];
            
             always @(*) begin
-                OX_RE = dspP_XX[DATA_WIDTH : 00];
-                OY_RE = dspP_YY[DATA_WIDTH : 00];
+                OX_RE = dspP_XX[DATA_WIDTH    : 00];
+                OY_RE = dspP_YY[DATA_WIDTH    : 00];
                 OX_IM = dspP_XX[DATA_WIDTH+24 : 24];
                 OY_IM = dspP_YY[DATA_WIDTH+24 : 24];
             end
@@ -886,23 +885,23 @@ module int_addsub_dsp48
         end 
         else if (DATA_WIDTH > 47) begin
             
-            wire [29 : 0] dspA_RE1,dspA_RE2,dspA_IM1,dspA_IM2;
-            wire [17 : 0] dspB_RE1,dspB_RE2,dspB_IM1,dspB_IM2;
-            wire [47 : 0] dspC_RE1,dspC_IM1;
-            reg  [47 : 0] dspC_RE2,dspC_IM2;
+            wire [29 : 0] dspA_RE1, dspA_RE2, dspA_IM1, dspA_IM2;
+            wire [17 : 0] dspB_RE1, dspB_RE2, dspB_IM1, dspB_IM2;
+            wire [47 : 0] dspC_RE1, dspC_IM1;
+            reg  [47 : 0] dspC_RE2, dspC_IM2;
             
-            wire [47 : 0] dspX_RE1,dspY_RE1,dspX_IM1,dspY_IM1;
-            wire [47 : 0] dspX_RE2,dspY_RE2,dspX_IM2,dspY_IM2;
+            wire [47 : 0] dspX_RE1, dspY_RE1, dspX_IM1, dspY_IM1;
+            wire [47 : 0] dspX_RE2, dspY_RE2, dspX_IM2, dspY_IM2;
             
-            wire [95 : 0] dspA_RE,dspA_IM,dspB_RE,dspB_IM;
+            wire [95 : 0] dspA_RE, dspA_IM, dspB_RE, dspB_IM;
             
-            wire dspC_XR,dspC_XI,dspC_YR,dspC_YI;
+            wire dspC_XR, dspC_XI, dspC_YR, dspC_YI;
         
             // -- port 48-bit data ----
-            assign dspA_RE = { {(96-DATA_WIDTH){IA_RE[DATA_WIDTH-1]}},IA_RE[DATA_WIDTH-1 : 0] };
-            assign dspA_IM = { {(96-DATA_WIDTH){IA_IM[DATA_WIDTH-1]}},IA_IM[DATA_WIDTH-1 : 0] };
-            assign dspB_RE = { {(96-DATA_WIDTH){IB_RE[DATA_WIDTH-1]}},IB_RE[DATA_WIDTH-1 : 0] };
-            assign dspB_IM = { {(96-DATA_WIDTH){IB_IM[DATA_WIDTH-1]}},IB_IM[DATA_WIDTH-1 : 0] };
+            assign dspA_RE = { {(96-DATA_WIDTH){IA_RE[DATA_WIDTH-1]}}, IA_RE[DATA_WIDTH-1 : 0] };
+            assign dspA_IM = { {(96-DATA_WIDTH){IA_IM[DATA_WIDTH-1]}}, IA_IM[DATA_WIDTH-1 : 0] };
+            assign dspB_RE = { {(96-DATA_WIDTH){IB_RE[DATA_WIDTH-1]}}, IB_RE[DATA_WIDTH-1 : 0] };
+            assign dspB_IM = { {(96-DATA_WIDTH){IB_IM[DATA_WIDTH-1]}}, IB_IM[DATA_WIDTH-1 : 0] };
 
             assign dspB_RE1 = dspB_RE[17 : 00];
             assign dspB_IM1 = dspB_IM[17 : 00];
